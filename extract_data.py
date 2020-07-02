1e4b1ff324dc164b683f56ff0dbd836c9a3b50e8
evgl <evgenii.li@yahoo.com>
2020-07-02 23:22:20 +0900

    Implemented steps 1-4 frequent_word_lists

--- extract_data.py
+++ extract_data.py
@@ -1,14 +1,76 @@
-from functions import extract_data
-
+import pandas as pd
+from functions import extract_data, count_notes_per_patient, logger, count_words_per_patient, find_frequent_word
+import json
+from nltk.stem import PorterStemmer
+"""
 # Variables
 
-# Extract data
+# Input vars --->
 disease_name = 'PNEUMONIA'
 database_path = '../MIMIC-III'
-#-----------
 
-alive_df, dead_df = extract_data(disease_name, database_path)
+patient_id_to_num_notes = {}
 
-print(alive_df.head())
-print('-'*60)
-print(dead_df.head())
+number_of_patients = {}
+note_appearance_counter = {}
+
+n_fold = float(3)
+threshold = float(0.02)
+frequent_word_lists = {}
+# Input vars ---<
+
+# Save dataframe ------------------->
+# alive_df.to_csv('alive_df.csv', index = False, header=True)
+# dead_df.to_csv('dead_df.csv', index = False, header=True)
+# Save dataframe -------------------<
+
+# Step 1
+# alive_df, dead_df = extract_data(disease_name, database_path)
+alive_df = pd.read_csv('alive_df.csv')
+dead_df = pd.read_csv('dead_df.csv')
+
+logger.info(f"Number of patients in label_0: {dead_df['SUBJECT_ID_x'].nunique()}")
+logger.info(f"Number of patients in label_1: {alive_df['SUBJECT_ID_x'].nunique()}")
+
+# Step 2
+logger.info("Count notes per patient...")
+patient_id_to_num_notes['label_0'] = count_notes_per_patient(dead_df)
+patient_id_to_num_notes['label_1'] = count_notes_per_patient(alive_df)
+
+# Step 3
+logger.info("Count words per patient...")
+number_of_patients['label_0'], note_appearance_counter['label_0'] = count_words_per_patient(dead_df, patient_id_to_num_notes['label_0'])
+number_of_patients['label_1'], note_appearance_counter['label_1'] = count_words_per_patient(alive_df, patient_id_to_num_notes['label_1'])
+
+# Step 4
+logger.info("Find frequent words...")
+frequent_word_lists = find_frequent_word(note_appearance_counter, number_of_patients, n_fold, threshold)
+logger.info(len(frequent_word_lists['label_0']))
+logger.info(len(frequent_word_lists['label_1']))
+
+with open('frequent_word_lists.json', 'w') as fp:
+    json.dump(frequent_word_lists, fp)
+"""
+# TODO: Step 2-4 can be merged into one
+
+with open('frequent_word_lists.json') as f:
+    frequent_word_lists = json.load(f)
+
+# Later can be done inside the find_frequent_word function
+FREQUENT_WORD_LIST = frequent_word_lists['label_0'] + frequent_word_lists['label_1']
+
+"""function description:
+generates frequent word set for the disease
+"""
+word_dict = {}
+word_id = 1
+stemmer = PorterStemmer()
+
+
+for word in FREQUENT_WORD_LIST:
+    if not word == "WORD":
+        word_dict[stemmer.stem(word.strip())] = word_id
+        word_id += 1
+             
+print("\nword dictionary created!\n")
+print(word_dict)
--- functions.py
+++ functions.py
@@ -8,7 +8,17 @@ from collections import Counter
 from tqdm import tqdm
 import sys
 import re
+import logging
 
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter(
+        '%(asctime)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+
+# define constants
 MIN_SEQ_LEN = 4
 USE_1_N_SEQ = 2
 
@@ -29,26 +39,33 @@ def extract_data(disease_name, database_path):
     with open('variables.json') as f:
         variable = json.loads(f.read())
 
-    print("Loading data. Please wait...")
+    logger.info("Loading data. Please wait...")
     admissions_df = pd.read_csv(os.path.join(database_path, "ADMISSIONS.csv"))
     noteevents_df = pd.read_csv(os.path.join(database_path, "NOTEEVENTS.csv")) 
 
-    print("Preprocessing...")
+    logger.info("Preprocessing...")
+    # Left join of two dataframes
     note_admiss_df_left = noteevents_df.merge(admissions_df, on='HADM_ID', how='left', indicator=True)
 
+    # Extract notes only for certain disease 
     disease_df = note_admiss_df_left.loc[note_admiss_df_left["DIAGNOSIS"] == disease_name, variable['disease_df']]
+    # Remove notes with discharge summary
     no_disch_df = disease_df.loc[disease_df["CATEGORY"] != 'Discharge summary', variable['disease_df']]
 
+    # Divide notes into two lables 
     alive_no_disch = no_disch_df[no_disch_df.DEATHTIME.isnull()]
     dead_no_disch = no_disch_df[no_disch_df.DEATHTIME.notnull()]
 
+    # Sort notes 
     alive_no_disch = alive_no_disch.sort_values(by=variable['order_df'])
     dead_no_disch = dead_no_disch.sort_values(by=variable['order_df'])
 
+    # Remove not needed fields
     alive_no_disch = alive_no_disch[variable['d_df']]
     dead_no_disch = dead_no_disch[variable['d_df']]
 
     return alive_no_disch, dead_no_disch
+
 
 def count_notes_per_patient(disease_df):
     patient_id_to_num_notes = {}
@@ -69,3 +86,96 @@ def count_notes_per_patient(disease_df):
     patient_id_to_num_notes[patient_id] = note_counter
     del patient_id_to_num_notes[-1]
     return patient_id_to_num_notes
+
+def count_words_per_patient(disease_df, patient_id_to_num_notes):
+    note_appearance_counter = {}
+    number_of_patients = 0 # number of patients
+    note_counter = 0
+# -----------
+    patient_id = -1
+    word_set = set()
+    note_event_counter = 0
+
+    # Iterate through each note
+    for index, row in tqdm(disease_df.iterrows(), total=disease_df.shape[0]):
+
+        patient_id_check = int(row['SUBJECT_ID_x'])
+    
+        # if patient id has changed, end sequence and start new sequence
+        if not patient_id == patient_id_check:
+            number_of_patients += 1
+            note_event_counter = 0
+        
+            for word in word_set:
+                if word in note_appearance_counter:
+                    note_appearance_counter[word] += 1
+                else:
+                    note_appearance_counter[word] = 1
+        
+            # reset word_set
+            word_set = set()
+        
+        # update patient id
+        patient_id = patient_id_check
+
+            
+        if patient_id_to_num_notes[patient_id_check] <= MIN_SEQ_LEN:
+            continue
+            
+        if note_event_counter < patient_id_to_num_notes[patient_id] // USE_1_N_SEQ:
+            note_event_counter += 1
+            continue
+                
+        note_counter += 1
+        note = re.sub(r'\[\*\*(.*?)\*\*\]|[_,\d\*:~=\.\-\+\\/]+', ' ', row['TEXT'])
+        tokenized_note = word_tokenize(note)
+        
+        
+        for word in tokenized_note:
+            stemmed_word = stemmer.stem(word.lower())
+            if not stemmed_word in stop_words:
+                word_set.add(stemmed_word)
+
+    return number_of_patients, note_appearance_counter
+
+def find_frequent_word(note_appearance_counter, number_of_patients, n_fold, threshold):
+
+    frequent_word_lists = {}
+    factor = {}
+    # calculate normalizing factor for each disease
+    note_sum = 0
+
+    # Count from two labels
+    for disease in number_of_patients:
+        note_sum += float(number_of_patients[disease])
+    
+    for disease in number_of_patients:
+        factor[disease] = number_of_patients[disease] / note_sum
+
+    # determine frequent word for each disease file
+    for disease in note_appearance_counter:
+        frequent_word_lists[disease] = []
+
+        logger.info(disease + " has " + str(len(note_appearance_counter[disease])) + " unique words!")
+
+        for word in note_appearance_counter[disease]:
+        
+            freq_check = True
+            for check_disease in note_appearance_counter:
+            
+                if not disease == check_disease:
+                    if word in note_appearance_counter[check_disease]:
+                        if not (note_appearance_counter[disease][word] / note_appearance_counter[check_disease][word] / factor[disease] * factor[check_disease] > n_fold \
+                            and note_appearance_counter[disease][word] > (number_of_patients[disease] * threshold)):
+
+                            freq_check = False
+                            break
+
+                    else:
+                        if not (note_appearance_counter[disease][word] > n_fold and note_appearance_counter[disease][word] > (number_of_patients[disease] * threshold)):
+                            freq_check = False
+                            break
+            if freq_check:
+                frequent_word_lists[disease].append((word))
+    
+    return frequent_word_lists
